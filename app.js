546e530dd67eaf66018e366561136b9a1d92e2a4
Scott <scott.tolksdorf@gmail.com>
2014-01-24 11:46:25 -0500

    Trying out the dogecoin api

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/public'));
 
 
+var dogecoin = require('node-dogecoin')()
+
+
+var test = dogecoin.getbalance('DLVhazsZNfZX3qAJ9wRfcKSqCwFnuQFCDt', function(err, result){
+	console.log(err, result);
+});
+
+console.log(test);
+
 app.get('/', function(req, res) {
 	res.render('../index.html');
+});
+
+app.get('/test', function(req, res){
+	dogecoin.getbalance('DLVhazsZNfZX3qAJ9wRfcKSqCwFnuQFCDt', function(err, result){
+		res.send(result);
+	});
+
 });
 
 var port = Number(process.env.PORT || 5000);
